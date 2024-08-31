8cb86c09f483db901a8909deb71f49ada3899f77
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-09-01 00:02:14 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   const {user}=useAuthContext();
   useEffect(() => {
     const fetchworkouts = async () => {
-      const res = await fetch("http://localhost:4000/api/workouts",{
+      const res = await fetch("https://mern-stack-backend-1wcw.onrender.com/",{
         headers:{'Authorization':`Bearer ${user.token}`},
       });
       const json = await res.json();
